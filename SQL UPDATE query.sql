3652283345095d8badd0992d1ef5420edd683d83
Rumps <bgoldsworthy96@gmail.com>
2014-12-17 22:55:53 +0000

    Final commit, bit messy but oh well

--- SQL UPDATE query.sql
+++
@@ -1,3 +0,0 @@
-UPDATE serverlist SET OS = "Linux" WHERE OS LIKE "Linux%";
-UPDATE serverlist SET OS = "AIX" WHERE OS LIKE "AIX%";
-UPDATE serverlist SET OS = "Windows" WHERE OS LIKE "Windows%";
---
+++ connect.php
@@ -0,0 +1,4 @@
+<?php
+	$conn = mysqli_connect(#NOPE#);
+	mysqli_select_db($conn, 'serverlist');
+?>
